fe68f17713eeebc0e1cfc9465904cae0723bf562
Jesse Freitas <freitas.jesse@gmail.com>
2013-08-01 17:27:15 -0400

    Code Clean Up

--- Breakpoint/Properties/AssemblyInfo.cs
+++ Breakpoint/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("breakpoint")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Breakpoint")]
+[assembly: AssemblyDescription("GitHub Issue Management Suite")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("breakpoint")]
+[assembly: AssemblyCompany("Jesse A. Freitas")]
+[assembly: AssemblyProduct("Breakpoint")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
